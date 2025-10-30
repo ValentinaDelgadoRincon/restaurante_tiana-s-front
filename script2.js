55bb89a6af73fa308083631f0a0e0c8288df6712
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-10-30 09:47:12 -0500

    fix: :bug: correction style
    correction style login
    
    By @Valenina

--- script2.js
+++ script2.js
@@ -550,23 +550,4 @@ const errorAdminMsg = document.getElementById("errorAdminMsg");
 
 
 
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
-
-const filterBtns = document.querySelectorAll(".filter-btn");
-filterBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    filterBtns.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
-  });
-});
 
